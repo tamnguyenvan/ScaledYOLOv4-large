232cf9f2b106195bde0be7c17afc8d8ece04811f
Nguyễn Văn Tâm <tamnguyenvan@users.noreply.github.com>
2021-02-23 15:15:02 +0700

    Fix class id

--- convert_coco_to_yolo.py
+++ convert_coco_to_yolo.py
@@ -11,6 +11,11 @@ from pycocotools.coco import COCO
 
 random.seed(42)
 
+index = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 27, 28, 31, 32, 33, 34,
+         35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63,
+         64, 65, 67, 70, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 84, 85, 86, 87, 88, 89, 90]
+coco91_to_coco80_index = {idx_91: idx_80 for idx_91, idx_80 in zip(index, range(80))}
+
 
 def load_data(image_dir, annotation_file):
     """Load data from dataset by pycocotools. This tools can be download from
@@ -55,7 +60,8 @@ def load_data(image_dir, annotation_file):
                            bboxes_data[2]/float(width),
                            bboxes_data[3]/float(height)]
             bboxes.append(bboxes_data)
-            labels.append(ann['category_id']-1)
+            class_id = ann['category_id']
+            labels.append(coco91_to_coco80_index[class_id])
 
         coco_data[img_id]['bboxes'] = bboxes
         coco_data[img_id]['labels'] = labels
